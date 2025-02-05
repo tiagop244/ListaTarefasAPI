89345b51dd055abb969fa3b6f33de46fd1473f1a
Tiago Pereira <tiagopereiraduarte8@gmail.com>
2025-02-05 09:20:57 -0300

    finalizar tarefa

--- ListaTarefasAPI/Controllers/TarefasController.cs
+++ ListaTarefasAPI/Controllers/TarefasController.cs
@@ -42,6 +42,12 @@ namespace ListaTarefasAPI.Controllers
             return tarefa;
         }
 
+        [HttpGet("concluidas")]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefasConcluidas()
+        {
+            return await _context.Tarefas.Where(t => t.Concluida == true).ToListAsync();
+        }
+
         // PUT: api/Tarefas/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
@@ -96,6 +102,38 @@ namespace ListaTarefasAPI.Controllers
 
             _context.Tarefas.Remove(tarefa);
             await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // PATCH: api/Tarefas/5/concluir
+        [HttpPatch("{id}/concluir")]
+        public async Task<IActionResult> ConcluirTarefa(int id)
+        {
+            var tarefa = await _context.Tarefas.FindAsync(id);
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            tarefa.Concluida = true;
+            _context.Entry(tarefa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TarefaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
             return NoContent();
         }
